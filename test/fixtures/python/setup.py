22e1020725a56e94117564d26782d6b53416ba3b
Brian J. Watson <brianwatson@google.com>
2016-09-21 18:01:11 -0700

    Fix error and test

--- test/fixtures/python/setup.py
+++ test/fixtures/python/setup.py
@@ -10,7 +10,7 @@ import setuptools
 from setuptools import setup, find_packages
 
 install_requires = [
-  'oauth2client>=0.4.1, <0.5.0',
+  'oauth2client>=0.4.1',
   'grpcio>=0.15.0, <0.16.0',
   'googleapis-common-protos[grpc]>=1.2.0, <2.0.0'
 ]
